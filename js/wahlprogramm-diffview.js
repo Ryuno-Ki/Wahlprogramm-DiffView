1793331818127b790f29e225030d4dc3f3fd98e5
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-09-25 23:23:52 +0200

    Toggle amendments.

--- js/wahlprogramm-diffview.js
+++ js/wahlprogramm-diffview.js
@@ -1,19 +1,20 @@
 (function(global) {
     "use strict";
-    var päas, node, i, len, päa, ol, li, amendment, j, len2, subAmendment, subItem;
+    var päas, node, choices, i, len, päa, ol, appendPäa, appendChoice, onUserChoice;
 
     päas = global.päas;
     node = document.getElementById("amendments");
-    ol = document.createElement("ol");
-    node.appendChild(ol);
-    for (i = 0, len = päas.length; i < len; i += 1) {
-        päa = päas[i];
-        console.log(päa);
+    choices = document.getElementById("choices");
+
+    appendPäa = function(list, päa) {
+        var li, amendment, i, len, subAmendment, subItem;
+
         li = document.createElement("li");
+        li.dataset.relatedTo = "PÄA" + päa.number;
         if (päa.hasModules) {
             amendment = document.createElement("ol");
-            for (j = 0, len2 = päa.amendment.length; j < len2; j += 1) {
-                subAmendment = päa.amendment[j];
+            for (i = 0, len = päa.amendment.length; i < len; i += 1) {
+                subAmendment = päa.amendment[i];
                 subItem = document.createElement("li");
                 subItem.appendChild(document.createTextNode(subAmendment.content));
                 amendment.appendChild(subItem);
@@ -22,8 +23,45 @@
             amendment = document.createTextNode(päa.amendment);
         }
         li.appendChild(amendment);
-        ol.appendChild(li);
+        list.appendChild(li);
+    };
+
+    appendChoice = function(container, päa) {
+        var item, label, checkbox;
+        item = document.createElement("p");
+        label = document.createElement("label");
+        checkbox = document.createElement("input");
+
+        item.appendChild(checkbox);
+        item.appendChild(label);
+        container.appendChild(item);
+
+        checkbox.type = "checkbox";
+        checkbox.id = "PÄA" + päa.number;
+        checkbox.checked = true;
+        label.setAttribute("for", "PÄA" + päa.number);
+        label.appendChild(document.createTextNode(päa.title));
+    };
+
+    onUserChoice = function(event) {
+        var checkbox, selector, item;
+        checkbox = event.target;
+        selector = checkbox.id;
+        item = amendments.querySelector("[data-related-to='" + selector + "']");
+        if (checkbox.checked) {
+            item.classList.remove("unselected");
+        } else {
+            item.classList.add("unselected");
+        }
+    };
+
+    ol = document.createElement("ol");
+    node.appendChild(ol);
+    for (i = 0, len = päas.length; i < len; i += 1) {
+        päa = päas[i];
+        appendPäa(ol, päa);
+        appendChoice(choices, päa);
     }
 
+    choices.addEventListener("change", onUserChoice);
 })(this)
-
