d940e81648cbeb2688988987a35f00243f77a4ec
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-09-26 22:17:06 +0200

    Accept or refuse amendments.

--- js/wahlprogramm-diffview.js
+++ js/wahlprogramm-diffview.js
@@ -1,10 +1,11 @@
 (function(global) {
     "use strict";
-    var päas, node, choices, i, len, päa, ol, appendPäa, appendChoice, onUserChoice;
+    var päas, node, choices, form, i, len, päa, ol, appendPäa, appendChoice, onUserChoice, buildAcceptRefuseFormOption, onSubmit, clearAmendment;
 
     päas = global.päas;
     node = document.getElementById("amendments");
     choices = document.getElementById("choices");
+    form = document.getElementById("accept-refuse");
 
     appendPäa = function(list, päa) {
         var li, amendment, i, len, subAmendment, subItem;
@@ -55,13 +56,58 @@
         }
     };
 
+    buildAcceptRefuseFormOption = function(form, päa) {
+        var select, option;
+
+        select = form.getElementsByTagName("select")[0];
+        option = document.createElement("option");
+        option.value = "PÄA" + päa.number;
+        option.textContent = "PÄA" + päa.number + ": " + päa.title;
+        select.appendChild(option);
+    };
+
+    clearAmendment = function(amendment, state) {
+        var choice;
+
+        choice = choices.querySelector("label[for='" + amendment.value + "']");
+        choice.classList.add(state.value);
+        choice.parentElement.querySelector("input").disabled = true;
+    };
+
+    onSubmit = function(event) {
+        var form, options, selected, inputs, checked;
+
+        form = event.target;
+        options = Array.prototype.slice.call(form.querySelectorAll("option"));
+        selected = options.reduce(function(former, latter) {
+            return former.selected ? former : latter;
+        });
+        inputs = Array.prototype.slice.call(form.querySelectorAll("input"));
+        checked = inputs.reduce(function(former, latter) {
+            return former.checked ? former : latter;
+        });
+        if (!checked.checked) {
+            checked = null;
+        }
+        clearAmendment(selected, checked);
+
+        selected.parentElement.removeChild(selected);
+        if (checked !== null) {
+            checked.checked = false;
+        }
+        event.preventDefault();
+        return false;
+    };
+
     ol = document.createElement("ol");
     node.appendChild(ol);
     for (i = 0, len = päas.length; i < len; i += 1) {
         päa = päas[i];
         appendPäa(ol, päa);
         appendChoice(choices, päa);
+        buildAcceptRefuseFormOption(form, päa);
     }
 
     choices.addEventListener("change", onUserChoice);
+    form.addEventListener("submit", onSubmit);
 })(this)
